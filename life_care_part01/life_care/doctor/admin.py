22edce1da3a2fea7b274f8474b3044ef6383281e
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-10 17:41:54 +0600

    added doctor model and registered it

--- life_care_part01/life_care/doctor/admin.py
+++ life_care_part01/life_care/doctor/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class Specialization(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+
+
+class Designation(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+
+
+admin.site.register(models.AvailableTime)
+admin.site.register(models.Specialization, Specialization)
+admin.site.register(models.Designation, Designation)
+admin.site.register(models.Doctor)
+admin.site.register(models.Review)
