643ff7d8273aab84020874bcb82fdf378e9eaf95
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-10 17:38:19 +0600

    added appointment model and registered it

--- life_care_part01/life_care/life_care/settings.py
+++ life_care_part01/life_care/life_care/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointment',
+    'contact_us',
+    'doctor',
+    'patient',
+    'service',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
