dfdfd211fcb25ea2929f8536b21ba233da1391fc
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-12 18:03:58 +0600

    added doctor pagination and filter

--- life_care_part03/life_care/doctor/models.py
+++ life_care_part03/life_care/doctor/models.py
@@ -15,7 +15,7 @@ class Specialization(models.Model):
 
 class Designation(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
 
     def __str__(self):
         return self.name
